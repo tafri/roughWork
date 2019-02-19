c478728da0aa8cd4a63bd517a72326c02b973710
Shobhit Asati <38384504+tafri@users.noreply.github.com>
2019-02-19 15:34:38 +0530

    Update countMapPartition.cpp

--- countMapPartition.cpp
+++ countMapPartition.cpp
@@ -1,36 +1,26 @@
 /*
 This program count the number of partitions in colored(number) matirx.
 */
-
-
 #include<iostream>
 #include<vector>
 using namespace std;
 
 vector<vector<int>> countMapPartition(vector<vector<int>> &A, int i, int j, int color){
-	
 	int row = A.size(), col = A[0].size();
 	A[i][j] = 0;
-	
-	if(j+1<col)																// go right
+	if(j+1<col)											// go right
 		if(A[i][j+1] == color)
 			countMapPartition(A, i, j+1, color);
-	
-	if(i+1<row)																// go down
+	if(i+1<row)											// go down
 		if(A[i+1][j] == color)
 			countMapPartition(A, i+1, j, color);
-	
-	if(j-1>=col)															// go left
+	if(j-1>=0)											// go left
 		if(A[i][j-1] == color)
 			countMapPartition(A, i, j-1, color);
-			
-	if(i-1>=row)															// go up
+	if(i-1>=0)											// go up
 		if(A[i-1][j] == color)
 			countMapPartition(A, i-1, j, color);
-
-
 	return A;
-
 }
 
 int main(){
@@ -39,7 +29,6 @@ int main(){
 	cin>>row>>col;
 	
 	vector<vector<int>> A (row, vector<int>(col));
-	
 	for(int i=0; i<row; i++)
 		for(int j=0; j<col; j++)
 			cin>>A[i][j];
@@ -51,8 +40,6 @@ int main(){
 				A = countMapPartition(A, i, j, color);
 				count++;
 			}
-	
 	cout<<count<<endl;
-
 return 0;
 }
